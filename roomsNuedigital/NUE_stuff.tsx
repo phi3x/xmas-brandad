3d0f002c9fb5db5c064a13ccc471a25882752d5e
Jürgen Krauß <juergen@es-ist-ein-krauss.de>
2021-06-08 16:34:13 +0200

    added hub for rooms (WIP)

---
+++ roomsNuedigital/NUE_stuff.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="NUE_stuff" tilewidth="32" tileheight="32" tilecount="252" columns="21">
+ <image source="../TilesNueDigital/NUE_stuff.png" width="698" height="384"/>
+</tileset>
