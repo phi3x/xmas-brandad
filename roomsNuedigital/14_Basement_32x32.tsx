e0628ff2957007e97d8e52cdc9b7f06d53ef54b0
Jürgen Krauß <juergen@es-ist-ein-krauss.de>
2021-06-10 17:17:36 +0200

    rooms hub (WIP)

---
+++ roomsNuedigital/14_Basement_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="14_Basement_32x32" tilewidth="32" tileheight="32" tilecount="800" columns="16">
+ <image source="14_Basement_32x32.png" width="512" height="1600"/>
+</tileset>
