373f63150bd0b37caaa7e36e06f84d5669c75875
Jürgen Krauß <juergen@es-ist-ein-krauss.de>
2021-06-10 10:49:37 +0200

    yellow train and info point added

---
+++ onboardingNuedigital/ZUG_START.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="ZUG_START" tilewidth="32" tileheight="32" tilecount="130" columns="13">
+ <image source="../TilesNueDigital/ZUG_START.png" width="446" height="324"/>
+</tileset>
