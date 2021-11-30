053320f9fdf49b10bec1af8027c0d30fcc202572
phi3x <91872577+phi3x@users.noreply.github.com>
2021-11-30 12:01:26 +0100

    GameStage 2 test PopUp

---
+++ exitRoom1/exitRoom1.js
@@ -0,0 +1,18 @@
+let helloWorldPopup;
+
+// Open the popup when we enter a given zone
+helloWorldPopup = WA.room.onEnterLayer("floorLayer").subscribe(() => {
+    WA.ui.openPopup("hintRoom1", 'Hello world!', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
+});
+
+// Close the popup when we leave the zone.
+WA.room.onLeaveLayer("floorLayer").subscribe(() => {
+    helloWorldPopup.close();
+})
